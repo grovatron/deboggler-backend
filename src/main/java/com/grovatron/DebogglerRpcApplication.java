d19130bf64ddc609337abc18edd87ade273df808
grovatron <grover.a.sundstrom@gmail.com>
2019-05-05 08:35:29 -0400

    Initial Commit

---
+++ src/main/java/com/grovatron/DebogglerRpcApplication.java
@@ -0,0 +1,13 @@
+package com.grovatron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebogglerRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebogglerRpcApplication.class, args);
+	}
+
+}
