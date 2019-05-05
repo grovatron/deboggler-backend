164a739fed39247eb5b303d29b13cea8624e8131
grovatron <grover.a.sundstrom@gmail.com>
2019-05-05 09:03:20 -0400

    Create ValidModifierValidator.

---
+++ src/main/java/com/grovatron/validation/ValidModifierValidator.java
@@ -0,0 +1,25 @@
+package com.grovatron.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ValidModifierValidator implements ConstraintValidator<ValidModifier, String> {
+
+	private String[] validModifiers;
+	
+	@Override
+	public void initialize(ValidModifier validModifier) {
+		validModifiers = validModifier.value();
+	}
+	
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		for (String modifier : validModifiers) {
+			if (value.equalsIgnoreCase(modifier)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
