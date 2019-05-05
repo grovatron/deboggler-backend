f0b17d4ffc1e5c7db26377156e9c4ea784821cd1
grovatron <grover.a.sundstrom@gmail.com>
2019-05-05 09:04:23 -0400

    Make adjustment...

--- src/main/java/com/grovatron/LetterInput.java
+++ src/main/java/com/grovatron/LetterInput.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.grovatron.validation.ValidLetter;
+import com.grovatron.validation.ValidModifier;
 
 import lombok.Data;
 
