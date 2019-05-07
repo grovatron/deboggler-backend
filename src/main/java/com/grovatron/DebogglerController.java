ac4bb62609fc6106847d7603721b9675aa714d45
grovatron <grover.a.sundstrom@gmail.com>
2019-05-06 21:26:13 -0400

    Allow cross-origin requests on post mapping

--- src/main/java/com/grovatron/DebogglerController.java
+++ src/main/java/com/grovatron/DebogglerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,8 @@ public class DebogglerController {
 
 	@Autowired
 	private DebogglerService debogglerFormService;
+	
+	@CrossOrigin
 	@PostMapping("/words")
 	List<Word> getWordList(@Valid @RequestBody DebogglerForm debogglerForm) {
 		return debogglerFormService.getWordList(debogglerForm);
